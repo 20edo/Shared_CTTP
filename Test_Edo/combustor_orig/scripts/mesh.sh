420ab02b39c74db551e83a6145a105f1aeb097c0
20edo <20edoardo@gmail.com>
2020-10-30 17:25:38 +0100

    Corrected snapyHexMeshDict to avoid reaching maximum number of cells due to outerWalls refinement. Now regions of the stl file are split in several patches, the outerWaals patch is less refined than the splitter. Layer addition to be tune depending on the problem to be solved

---
+++ Test_Edo/combustor_orig/scripts/mesh.sh
@@ -0,0 +1,25 @@
+# This contains the commands needed to mesh the combustor
+
+# Define variables
+np='4'	# Number of processors
+
+# Lauch routine
+
+mkdir log
+
+blockMesh 2>&1 | tee log/blockMesh.log
+
+rotateMesh '(0 0 1)' '(0 1 0)' -noZero  2>&1 | tee log/rotateMesh.log
+
+
+surfaceCheck constant/triSurface/combustor_mm.stl 2>&1 | tee log/surfaceCheck.log
+
+surfaceFeatures -dict system/surfaceFeaturesDict 2>& tee log/surfaceFeatures.log
+
+decomposePar 2>&1 | tee log/decomposePar.log
+
+
+mpirun -np $np snappyHexMesh -parallel 2>&1 | tee log/snappyHexMesh.log
+
+checkMesh 2>&1 | tee log/checkMesh.log
+
