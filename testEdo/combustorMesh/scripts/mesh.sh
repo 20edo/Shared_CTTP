ba9dfb4916892c6d963e0df5ed5abf41e26ffe19
20edo <20edoardo@gmail.com>
2020-11-15 10:59:57 +0100

    Improved meshing performances by tuning some parameters, added the possibility of radial grading for the cylindrical part of the mesh. There is still work to do on layer addition

--- testEdo/combustorMesh/scripts/mesh.sh
+++ testEdo/combustorMesh/scripts/mesh.sh
@@ -2,11 +2,14 @@ np=4	# Number of processors
 
 mkdir log
 
-blockMesh					2>&1 | tee log/blockMesh.log
-surfaceFeatures					2>&1 | tee log/surfaceFeatures.log
-decomposePar					2>&1 | tee log/decomposePar.log
-mpirun -np $np	snappyHexMesh	-parallel	2>&1 | tee log/snappyHexMesh.log
-mpirun -np $np	checkMesh	-parallel	2>&1 | tee log/checkMesh.log
+surfaceCheck	constant/triSurface/combustor.stl	2>&1 | tee log/surfaceCheck.log
+mv problemFaces *.obj constant/triSurface
+blockMesh						2>&1 | tee log/blockMesh.log
+rotateMesh '(0 0 1)' '(0 1 0)' -noZero  		2>&1 | tee log/rotateMesh.log
+surfaceFeatures						2>&1 | tee log/surfaceFeatures.log
+decomposePar						2>&1 | tee log/decomposePar.log
+mpirun -np $np	snappyHexMesh	-parallel		2>&1 | tee log/snappyHexMesh.log
+mpirun -np $np	checkMesh	-parallel		2>&1 | tee log/checkMesh.log
 
 touch	combustor.foam
 
