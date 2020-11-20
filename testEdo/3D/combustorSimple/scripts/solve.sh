ffcbb938bdf62bfc9c270b710f33f863161dfd7d
20edo <20edoardo@gmail.com>
2020-11-20 20:04:24 +0000

    Post-processing WORK IN PROGRESS

--- testEdo/3D/combustorSimple/scripts/solve.sh
+++ testEdo/3D/combustorSimple/scripts/solve.sh
@@ -1,6 +1,6 @@
 np=4	# Number of processors
 reconstructParMesh			2>&1	| tee 	log/reconstructParMesh.log
-cp 0/* 3
-decomposePar	-latestTime		2>&1	| tee	log/decomposePar2.log
+cp 0*/* 3
+decomposePar	-force	-latestTime	2>&1	| tee	log/decomposePar2.log
 mpirun -np $np simpleFoam -parallel 	2>&1	| tee	log/simpleFoam.log
 recontructPar				2>&1	| tee	log/recontructPar.log
