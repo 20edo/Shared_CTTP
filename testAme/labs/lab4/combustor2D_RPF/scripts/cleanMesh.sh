45d3dab122de4bcca4616e7bffe011998c179221
Deobaq <amedeo.santini@mail.polimi.it>
2020-11-28 20:42:48 +0100

    combustor2D_RPF update
    cleaned case

--- testAme/labs/lab4/combustor2D_RPF/scripts/cleanMesh.sh
+++
@@ -1,6 +0,0 @@
-rm -rf constant/extendedFeatureEdgeMesh
-rm -f constant/trisurface/{problemFaces,*.obj,*.vtk,*.eMesh}
-rm -r constant/polyMesh
-rm -rf processor* log dynamicCode
-rm -rf post*
-rm -rf VTK
--- testAme/labs/lab4/combustor2D_RPF/scripts/run.sh
+++
@@ -1,2 +0,0 @@
-./scripts/mesh.sh
-./scripts/solve.sh
