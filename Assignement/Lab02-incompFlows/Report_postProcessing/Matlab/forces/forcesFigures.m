15df335f856dd575ca60ba6b58404c5117206f06
Deobaq <amedeo.santini@mail.polimi.it>
2020-12-08 13:42:06 +0100

    Figures update

--- Assignement/Lab02-incompFlows/Report_postProcessing/Matlab/forces/forcesFigures.m
+++ Assignement/Lab02-incompFlows/Report_postProcessing/Matlab/forces/forcesFigures.m
@@ -7,33 +7,46 @@ clc
 data = load('forcesPimple.txt');
 
 t = data(:,1);
-drag = data(:,3);
+drag_pres = data(:,3);
+drag_tot = data(:,3)+data(:,6);
 
 figure()
-plot(t,drag)
+hold on
+plot(t,drag_pres)
+plot(t,drag_tot)
 xlabel('Time $[s]$','Interpreter','Latex')
 ylabel('Drag $[N]$','Interpreter','Latex')
+legend('Drag','Total Force','Location','SouthEast','Interpreter','Latex')
 
 %% pisoDrag
 
 data = load('forcesPiso.txt');
 
 t_P = data(:,1);
-drag_P = data(:,3);
+drag_pres_P = data(:,3);
+drag_tot_P = data(:,3)+data(:,6);
 
 figure()
-plot(t_P,drag_P)
+hold on
+plot(t_P,drag_pres_P)
+plot(t_P,drag_tot_P)
 xlabel('Time $[s]$','Interpreter','Latex')
 ylabel('Drag $[N]$','Interpreter','Latex')
+legend('Drag','Total Force','Location','SouthEast','Interpreter','Latex')
+ylim([-0.04 0.04])
 
 %% simpleDrag
 
 data = load('forcesSimple.txt');
 
 t_S = data(:,1);
-drag_S = data(:,3);
+drag_pres_S = data(:,3);
+drag_tot_S = data(:,3)+data(:,6);
 
 figure()
-plot(t_S,drag_S)
-xlabel('Time $[s]$','Interpreter','Latex')
+hold on
+plot(t_S,drag_pres_S)
+plot(t_S,drag_tot_S)
+xlabel('Iteration Number $[-]$','Interpreter','Latex')
 ylabel('Drag $[N]$','Interpreter','Latex')
+legend('Drag','Total Force','Location','SouthEast','Interpreter','Latex')
